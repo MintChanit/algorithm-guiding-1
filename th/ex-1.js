b525ec276b24a8d39008a2eb0b7040eadac090b6
MINT <mint_chanit@hotmail.com>
2025-04-07 08:11:39 +0700

    Answer: algorithm guiding-1

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,9 +1,24 @@
 const moveZeroes = function (nums) {
-  //Start coding here
+  let notZeroNumber = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[notZeroNumber] = nums[i];
+      notZeroNumber++;
+    }
+  }
+
+  for (let i = notZeroNumber; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+
+  return nums;
 };
+
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
 console.log(result1); // [1,3,12,0,0]
 
 const result2 = moveZeroes([0]);
 console.log(result2); // [0]
+
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,6 +1,31 @@
 const romanToInt = function (s) {
-  //Start coding here
+  const roman = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const currentRoman = roman[s[i]];
+    const nextRoman = roman[s[i + 1]];
+
+    if (nextRoman > currentRoman) {
+      result += nextRoman - currentRoman;
+      i++;
+    } else {
+      result += currentRoman;
+    }
+  }
+
+return result;
 };
+
 
 const result1 = romanToInt("III"); // 3
 console.log(result1);
